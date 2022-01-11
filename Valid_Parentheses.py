702e75f7fdda8dcc633442314eab40d69a79ec52
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-10 23:03:04 -0500

    Update Valid_Parentheses.py

--- Valid_Parentheses.py
+++ Valid_Parentheses.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        d = {'(': ')', '{': '}', '[': ']'}
+        for i in s:
+            if stack and d.get(stack[-1]) == i:
+                stack.pop()
+            else:
+                stack.append(i)
+        return not bool(stack)
