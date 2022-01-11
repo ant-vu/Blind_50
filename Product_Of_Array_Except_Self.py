66fce321f509e686f7598b2f9961517ea0d98145
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-11 16:22:00 -0500

    Update Product_Of_Array_Except_Self.py

--- Product_Of_Array_Except_Self.py
+++ Product_Of_Array_Except_Self.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        answer = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+            right[-i - 1] = right[-i] * nums[-i]
+        for i in range(len(nums)):
+            answer[i] = left[i] * right[i]
+        return answer
