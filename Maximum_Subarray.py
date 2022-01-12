76b286d29b9fb2cdf85dbd5e75a54bab7e1051a5
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-12 14:06:11 -0500

    Update Maximum_Subarray.py

--- Maximum_Subarray.py
+++ Maximum_Subarray.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur_max, max_till_now = 0, -inf
+        for i in nums:
+            cur_max = max(i, cur_max + i)
+            max_till_now = max(max_till_now, cur_max)
+        return max_till_now
