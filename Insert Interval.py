6db37dda1e213364636f1dd08dd021204a0ec710
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-02-12 17:14:21 -0500

    Update Insert Interval.py

--- Insert Interval.py
+++ Insert Interval.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []        
+        s, e = newInterval
+        for i in range(len(intervals)):
+            if (intervals[i][0] <= s <= intervals[i][1]) or (intervals[i][0] <= e <= intervals[i][1]) or (s <= intervals[i][0] and e >= intervals[i][1]):
+                s = min(s, intervals[i][0])
+                e = max(e, intervals[i][1])
+            else:
+                res.append(intervals[i])
+        res.append([s,e])
+        return sorted(res)
